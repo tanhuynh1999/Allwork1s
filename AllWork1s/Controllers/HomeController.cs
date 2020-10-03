1f55a526731f062f9fbbd3acb5e73914923dac4f
tanhuynh1999 <54873015+tanhuynh1999@users.noreply.github.com>
2020-10-04 01:35:23 +0700

    Add project files.

---
+++ AllWork1s/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AllWork1s.Models;
+
+namespace AllWork1s.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataWorkEntities db = new DataWorkEntities();
+        public ActionResult Index()
+        {
+            return View(db.Works.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public PartialViewResult Css()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ AllWork1s/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AllWork1s.MvcApplication" Language="C#" %>
